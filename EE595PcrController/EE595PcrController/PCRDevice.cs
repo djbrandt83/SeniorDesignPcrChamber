b1ac6e1c164dee1bad31ed6e1b0acf40d8a560a9
djbrandt83 <40121156+djbrandt83@users.noreply.github.com>
2018-11-03 13:26:44 -0500

    Added grpahics or Experimental Status Tab.

--- EE595PcrController/EE595PcrController/PCRDevice.cs
+++ EE595PcrController/EE595PcrController/PCRDevice.cs
@@ -8,16 +8,42 @@ namespace EE595PcrController
 {
     class PCRDevice
     {
-        private static String ConnectedMessage = "No device is currently connected";
-        private static String DisconnectedMessage = "The device is currently connected";
+        private static readonly String ConnectedMessage = "No device is currently connected";
+        private static readonly String DisconnectedMessage = "The device is currently connected";
 
         public String ConnectionMessage;
         public Boolean DeviceOnline;
+        public Int32 OutputTemperature;
+        public Int32 ReferenceTemperature;
 
         public PCRDevice()
         {
             ConnectionMessage = DisconnectedMessage;
             DeviceOnline = false;
+            OutputTemperature = 20;
+            ReferenceTemperature = 20;
+        }
+
+        public void StartStopExperiment(Boolean experimentRunning)
+        {
+            if(!experimentRunning)
+            {
+                StartExperiment();
+            }
+            else
+            {
+                StopExperiment();
+            }
+        }
+
+        private void StopExperiment()
+        {
+
+        }
+
+        private void StartExperiment()
+        {
+
         }
     }
 }
